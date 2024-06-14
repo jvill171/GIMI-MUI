9b65f078a36187f8182c1adb5eeafb23083fd839
jvill171 <41810008+jvill171@users.noreply.github.com>
2024-06-13 21:42:35 -0500

    Added docstrings and restructured some class methods

--- GIMI-MUI.py
+++ GIMI-MUI.py
@@ -6,6 +6,11 @@ import sys, os
 
 class Main(QMainWindow):
     def __init__(self):
+        """
+        Initialize the Main window.
+
+        Load the UI file, initialize QSettings, and set up the initial UI settings.
+        """
         super(Main, self).__init__()
         loadUi("main.ui", self)
         self.settings = QSettings("github.io/jvill171", "GIMI ModUI")  # Initialize QSettings
@@ -13,6 +18,12 @@ class Main(QMainWindow):
     
 
     def initUI(self):
+        """
+        Set up the initial UI settings.
+
+        Connect signals (e.g., browseButton clicked signal), disable maximize button,
+        and set up any initial settings using setupSettings().
+        """
         self.setWindowTitle('GIMI ModUI')
         self.browseButton.clicked.connect(self.openFileDialog)
         # Disable maximize button
@@ -25,36 +36,53 @@ class Main(QMainWindow):
 
 
     def setupSettings(self):
+        """
+        Set up application settings on startup.
+
+        Load the last selected directory from QSettings, display it in selectedDirectoryLabel,
+        set tooltips, and initialize mod directories using setModDirs().
+
+        """
 
         # Load last selected directory on startup, default current directory
         modding_dir = self.settings.value("last_directory", os.getcwd())
         self.selectedDirectoryLabel.setText(modding_dir)
         self.selectedDirectoryLabel.setToolTip(modding_dir)
-        self.setModdingDirs(modding_dir=modding_dir)
+        self.setModDirs(modding_dir=modding_dir)
 
 
-    def setModdingDirs(self, modding_dir):
+    def setModDirs(self, modding_dir):
+        """
+        Set directories for "Active" and "Inactive" mods.
+
+        Args:
+            modding_dir (str): Directory path where "Active" and "Inactive" directories are located.
+        """
+
         # Set locations of expected "Active" & "Inactive" directories
         active_dir, inactive_dir = modding_dir + "/Active", modding_dir + "/Inactive"
         # Verify "Active" & "Inactive" directories exist
         active_exists, inactive_exists = os.path.exists(active_dir), os.path.exists(inactive_dir)
 
-        if active_exists:
+        if active_exists: 
             self.listDirectories(active_dir, self.activeModList)
-        else:
-            self.activeModList.setEnabled(False)
-
         if inactive_exists:
             self.listDirectories(inactive_dir, self.inactiveModList)
-        else:
-            self.inactiveModList.setEnabled(False)
-        
-        if not (active_exists and inactive_exists):
-            self.addModButton.setEnabled(False)
-            self.removeModButton.setEnabled(False)
-        
+
+        # Enabled/Disable appropriate widgets based on if directories exists
+        self.activeModList.setEnabled(active_exists)
+        self.inactiveModList.setEnabled(inactive_exists)
+        self.addModButton.setEnabled(active_exists and inactive_exists)
+        self.removeModButton.setEnabled(active_exists and inactive_exists)
 
     def openFileDialog(self):
+        """
+        Open a file dialog to select the Mods folder.
+
+        Update selectedDirectoryLabel with the chosen directory path, save it to QSettings,
+        and update mod directories using setModDirs().
+        """
+
         options = QFileDialog.Options(QFileDialog.DontUseNativeDialog | QFileDialog.ShowDirsOnly)   # Use Qt-specific dialog | Only show directories
         directory = QFileDialog.getExistingDirectory(self, "Select Mods Folder", options=options)
 
@@ -64,9 +92,18 @@ class Main(QMainWindow):
             self.selectedDirectoryLabel.setToolTip(directory)
             # Save selected directory to QSettings
             self.settings.setValue("last_directory", directory)
+            self.setModDirs(modding_dir=directory)
     
 
     def listDirectories(self, directory, list_widget):
+        """
+        List directories within a specified directory and populate a QListWidget.
+
+        Args:
+            directory (str): Directory path to list directories from.
+            list_widget (QListWidget): QListWidget to populate with directory names.
+        """
+
         list_widget.clear()
         try:
             for item_name in os.listdir(directory):
